1a116d5def73d129454d1055ea3dd5f08e25fa90
Rodrigo <rodrigoalbues@hotmail.com>
2024-05-04 21:12:43 -0300

    ex079 solved

--- Mundo-3/ex079.py
+++ Mundo-3/ex079.py
@@ -1,1 +1,27 @@
 """Crie um prog onde o usuário possa digitar vários valores numéricos (perguntar se deseja continuar) e cadastre-os em uma lista. Caso o num já exista lá dentro, ele não será add. No final, serão exibidos todos os valores únicos digitados, em ordem crescente."""
+
+valores = []
+print("=" * 80)
+while True:
+    valor = input("Digite um valor: ")
+    if valor.isnumeric():
+        valor = int(valor)
+        if valor not in valores:
+            valores.append(valor)
+            print("Valor cadastrado com sucesso...")
+            print("")
+    else:
+        print("Informação inválida!")
+    continuar = input("Deseha contiuar? [S/N] ").upper().strip()
+    print("")
+
+    if continuar and continuar in "SN":
+        if continuar == "N":
+            break
+    else:
+        print("Informação inválida!")
+        break
+    print("-" * 80)
+print("")
+print(f"Você digitou os valores {valores}")
+print("=" * 80)
