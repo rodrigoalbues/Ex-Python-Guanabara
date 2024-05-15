b246ef5532f1f0baf411c943785001422fec7239
Rodrigo <rodrigoalbues@hotmail.com>
2024-05-14 21:07:23 -0300

    ex090 e ex091 resolvidos

--- Mundo-3/ex090.py
+++ Mundo-3/ex090.py
@@ -1,1 +1,20 @@
 """Faça um prog que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela."""
+
+print("=" * 60)
+nome = input("Nome: ")
+media = input(f"Média de {nome}: ")
+if media and nome:
+    media = float(media)
+    if media < 7:
+        situacao = "Reprovado"
+    else:
+        situacao = "Aprovado"
+    aluno = {"Nome": nome, "Media": media, "situacao": situacao}
+    print(
+        f"""O nome do aluno é: {aluno["Nome"]}.
+Sua média é: {aluno["Media"]}.
+Sua situação é: {aluno["situacao"]}"""
+    )
+else:
+    print("Informação inválida!")
+print("=" * 60)
--- Mundo-3/ex091.py
+++ Mundo-3/ex091.py
@@ -1,1 +1,21 @@
 """Crie um programa onde 4 jogoadores joguem um dado e tenham resultados aleatórios em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencendor tirou o maior nr no dado."""
+
+from random import randint
+
+print("=" * 60)
+jogadores = {
+    "jogador 1": randint(1, 6),
+    "jogador 2": randint(1, 6),
+    "jogador 3": randint(1, 6),
+    "jogador 4": randint(1, 6),
+}
+for j, v in jogadores.items():
+    print(f" O {j} tirou {v}")
+print("-" * 60)
+ranking = sorted(jogadores.items(), key=lambda x: x[1], reverse=True)
+
+print("Ranking dos jogadores:")
+print("-" * 60)
+for i, (jogador, valor) in enumerate(ranking, start=1):
+    print("{}º lugar: Jogador {} com {} pontos.".format(i, jogador, valor))
+print("=" * 60)
