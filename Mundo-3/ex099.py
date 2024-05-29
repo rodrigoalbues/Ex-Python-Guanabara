8dbe0f572ba23d59db6dfa4d457876f08b729823
Rodrigo <rodrigoalbues@hotmail.com>
2024-05-29 20:51:23 -0300

    ex099 resolvido

--- Mundo-3/ex099.py
+++ Mundo-3/ex099.py
@@ -1,6 +1,29 @@
 """Faça um prog que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
 Seu prog tem que analisar todos os valores e dizer qual deles é o maior."""
 
+from time import sleep
+
 
 def maior(*num):
-    maior = float("-inf")
+    cont = maior = 0
+    print("=" * 60)
+    print("Analisando os valores passados... ")
+    for valor in num:
+        print(f"{valor}", end=" ", flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f"Foram informados {cont} valores ao todo.")
+    print(f"O maior valor informado foi {maior}.")
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
+print()
