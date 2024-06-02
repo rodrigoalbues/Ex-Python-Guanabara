5e5de3ac1046c70973ec6a113fd54d62a70ecb08
Rodrigo <rodrigoalbues@hotmail.com>
2024-06-02 19:31:30 -0300

    ex104 resolvido

--- Mundo-3/ex104.py
+++ Mundo-3/ex104.py
@@ -1,1 +1,19 @@
-'''Crie um prog que tenha a função leiaInt(), que vai funcionar de forma semelhante à função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.'''
+"""Crie um prog que tenha a função leiaInt(), que vai funcionar de forma semelhante à função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico."""
+
+
+def leiaInt(msg):
+    while True:
+        n = input(msg)
+        if n.isnumeric():
+            n = int(n)
+            break
+        else:
+            print("\033[0;31mERRO! Digite um número inteiro válido.\033[m]")
+
+    return n
+
+
+print("=" * 60)
+num = leiaInt("Digite um número inteiro: ")
+print(f"Você acabou de digitar o número {num}.")
+print("=" * 60)
